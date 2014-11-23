4921cfdd4d830cc28cc010519ca9ccd8571a5483
EtienneK <e@ek.pe>
2014-11-23 15:50:29 +0200

    Adding a REST delete

--- src/main/java/com/etiennek/elec/api/ApiController.java
+++ src/main/java/com/etiennek/elec/api/ApiController.java
@@ -1,6 +1,7 @@
 package com.etiennek.elec.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,11 @@ public class ApiController {
   @RequestMapping(value = "/api/readings")
   public Iterable<ElecEntry> list() {
     return repo.findAll();
+  }
+
+  @RequestMapping(value = "/api/readings/delete/{id}")
+  public void delete(@PathVariable long id) {
+    repo.delete(id);
   }
 
 }
