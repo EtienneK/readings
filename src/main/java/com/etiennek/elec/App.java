a4987fd7fd73215a30438bfc08fece5fcac38cb4
EtienneK <e@ek.pe>
2014-11-23 11:49:00 +0200

    trying to get this to work on heroku

--- src/main/java/com/etiennek/elec/App.java
+++ src/main/java/com/etiennek/elec/App.java
@@ -2,13 +2,15 @@ package com.etiennek.elec;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,25 +20,64 @@ import org.springframework.context.annotation.Configuration;
 public class App {
 
   public static void main(String[] args) throws Exception {
+    if (System.getenv("DATABASE_URL") != null) {
+      args = herokuConfiguration(args);
+    }
     SpringApplication.run(App.class, args);
   }
 
-  @Bean
-  public Connection getConnection() throws URISyntaxException, SQLException {
-    String databaseUrl = System.getenv("DATABASE_URL");
-
-    System.out.println("XXXXXXXXXXXXX " + databaseUrl + " XXXXXXXXXXXXX");
-
-    if (databaseUrl == null) {
-      return null;
+  private static String[] herokuConfiguration(String[] args) {
+    URI dbUri = null;
+    try {
+      dbUri = new URI(System.getenv("DATABASE_URL"));
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    }
+    String[] credentials = dbUri.getUserInfo()
+                                .split(":");
+    String username = credentials[0];
+    String password;
+    if (credentials.length > 1) {
+      password = credentials[1];
+    } else {
+      password = "";
     }
 
-    URI dbUri = new URI(System.getenv("DATABASE_URL"));
-    String username = dbUri.getUserInfo()
-                           .split(":")[0];
-    String password = dbUri.getUserInfo()
-                           .split(":")[1];
-    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
-    return DriverManager.getConnection(dbUrl, username, password);
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+    List<String> argList = new ArrayList<>(Arrays.asList(args));
+    argList.add("--dsUrl=" + dbUrl);
+    argList.add("--dsUsername=" + username);
+    argList.add("--dsPassword=" + password);
+
+    return argList.toArray(new String[argList.size()]);
+  }
+
+  public DataSource dataSource() {
+    String databaseUrl = System.getenv("DATABASE_URL");
+    URI dbUri = null;
+    try {
+      dbUri = new URI(databaseUrl);
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    }
+    String[] credentials = dbUri.getUserInfo()
+                                .split(":");
+    String username = credentials[0];
+    String password;
+    if (credentials.length > 1) {
+      password = credentials[1];
+    } else {
+      password = "";
+    }
+
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+    BasicDataSource basicDataSource = new BasicDataSource();
+    basicDataSource.setUrl(dbUrl);
+    basicDataSource.setUsername(username);
+    basicDataSource.setPassword(password);
+
+    return basicDataSource;
   }
 }
